fa99c9aec0021d6c76322da228116995dd8802c5
Mishu <124211865+mishhuu@users.noreply.github.com>
2023-05-04 08:16:54 +0000

    Updated redes.js

--- Commands/Public/Fun/redes.js
+++ Commands/Public/Fun/redes.js
@@ -38,6 +38,51 @@ module.exports = {
   async execute(interaction, client) {
     const red_social = interaction.options.getString("red");
     const nombre_de_usuario = interaction.options.getString("user");
+    let fondo = "https://i.imgur.com/y2XDHuy.png";
+    let texto_user = `User: ${interaction.user.tag}`;
+    const canvas = Canvas.createCanvas(1200, 400);
+    const ctx = canvas.getContext("2d");
+    const background = await Canvas.loadImage(`${fondo}`);
+    let myAvatar = interaction.member.displayAvatarURL({ extension: "png" });
+    myAvatar = await Canvas.loadImage(myAvatar);
+    //dibujar el fondo
+    ctx.save();
+    ctx.filter = "blur(0px)";
+    ctx.drawImage(background, 0, 0, 1200, 400);
+    ctx.restore();
+    ctx.globalAlpha = 0.5;
+    ctx.fillRect(16, 16, 1, 1);
+    ctx.globalAlpha = 1;
+    //Dibujar logo del autor
+    ctx.save();
+    ctx.beginPath();
+    const radius1 = 145;
+    ctx.arc(200, 200, radius1, 0, 2 * Math.PI);
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "#FFFFFF";
+    ctx.stroke();
+    ctx.clip();
+    ctx.drawImage(myAvatar, 45 + 10, 45 + 10, 290, 290);
+    ctx.restore();
+
+    //Dibujar corazón
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font = "80px Arial";
+    ctx.fillText(texto_user, 600, 200);
+    let foto_red_social;
+    if (red_social === "instagram"){
+      foto_red_social = "https://1000marcas.net/wp-content/uploads/2019/11/Instagram-Logo.png";
+    } else if (red_social === "tiktok") {
+      foto_red_social = "https://static.vecteezy.com/system/resources/previews/018/930/463/original/tiktok-logo-tikok-icon-transparent-tikok-app-logo-free-png.png";
+    } else if (red_social === "twitter") {
+      foto_red_social = "https://static.vecteezy.com/system/resources/previews/016/716/467/non_2x/twitter-icon-free-png.png";
+    }
+    foto_red_social = await Canvas.loadImage(foto_red_social);
+    const file = new AttachmentBuilder(await canvas.encode("png"), {
+      name: "y2XDHuy.png",
+    });
     interaction.reply({
       content: `${red_social}: \nUser: ${nombre_de_usuario} \nDiscord Tag: ${interaction.user.tag}`,
     });
